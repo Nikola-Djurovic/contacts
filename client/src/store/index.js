729811b0494fa3b4adebd17d4b9419c0434cd372
Nikola-Djurovic <ndjurovic0416rn@raf.rs>
2021-08-08 09:25:38 +0200

    added vuex store

---
+++ client/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+export default new Vue.Store(
+    {
+        state:{
+
+        },
+        mutations:{
+
+        },
+        actions:{},
+        modules:{}
+    }
+)
